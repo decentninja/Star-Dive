f2441ef2e992fc023c5061dcabf4a2056b233cea
andreas <encrypt.wave@gmail.com>
2013-10-17 11:30:15 +0200

    Window resize

--- graphics.js
+++ graphics.js
@@ -4,21 +4,26 @@
         antialias: true,
     })
     renderer.autoClear = false
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
     var background = new THREE.Scene()
     var foreground = new THREE.Scene()
-    renderer.setSize(window.innerWidth, window.innerHeight)
     
     world.init(foreground, background)
     ship.init(foreground, background)
+
+    var winResize = new THREEx.WindowResize(renderer, ship.camera)
     
     function update() {
         renderer.clear()
+
         ship.camera.far = 10000000
         ship.camera.updateProjectionMatrix()
         renderer.render(background, ship.camera)
         ship.camera.far = 1000
         ship.camera.updateProjectionMatrix()
         renderer.render(foreground, ship.camera)
+
         requestAnimationFrame(update)
     }
     update();
